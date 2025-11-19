36d89f11b9821ceda37481558391fc58e423f9f5
Aarón Ayllon C <aaronayllonc@gmail.com>
2025-11-18 21:51:45 -0400

    Arreglo Scroll

--- docs/nosotros/nosotro.js
+++ docs/nosotros/nosotro.js
@@ -24,3 +24,17 @@ document.addEventListener('click', () => {
         menu.style.display = 'none';
     });
 });
+// boton Scroll
+const btnTop = document.getElementById('btntop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {  // aparece después de bajar 200px
+    btnTop.classList.add('show');
+  } else {
+    btnTop.classList.remove('show');
+  }
+});
+
+btnTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
