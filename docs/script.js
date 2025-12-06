5a2d325512f89bd695ac152cb6f114055da3db2f
espinozavargasabrahamisec-png <tu-email-principal@gmail.com>
2025-12-06 13:26:18 +0300

    Cambios  de  la encuesta

--- docs/script.js
+++ docs/script.js
@@ -1,20 +1,24 @@
-/* ===============================
-   🚀 Enviar formulario con Fetch
-   =============================== */
-function enviarFormulario(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
 
-    fetch('guardar_comentario.php', {
-        method: 'POST',
-        body: formData
+<script src="https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js"></script>
+
+
+  (function() {
+    emailjs.init("contacto@aureamed.com.bo"); // <- Lo reemplazas
+  })();
+
+  document.getElementById('form-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.send("TU_SERVICE_ID", "TU_TEMPLATE_ID", {
+      nombre: document.getElementById("nombre").value,
+      correo: document.getElementById("correo").value,
+      asunto: document.getElementById("asunto").value,
+      mensaje: document.getElementById("mensaje").value
     })
-    .then(response => response.text())
-    .then(data => {
-        document.body.innerHTML = data;
-    })
-    .catch(error => {
-        alert('❌ Error: ' + error);
+    .then(function() {
+        alert("Mensaje enviado con éxito");
+    }, function(error) {
+        alert("Error al enviar: " + JSON.stringify(error));
     });
-}
+  });
 
